42d4efc6fe29ed38455edccd8c7ff5b0febcfd6e
Zhongqi Shan <60977360+Sabrave@users.noreply.github.com>
2019-02-26 00:07:35 +0800

    Blank C++ Project Template

---
+++ Source/KaiMart/KaiMartGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KaiMartGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KAIMART_API AKaiMartGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
